0201f2689f01c06e8e28c4fe3752862884dc64c0
MD DANISH JAWED <95688212+Jaweddanish19@users.noreply.github.com>
2021-12-19 03:35:14 +0530

    Add files via upload

---
+++ SIMPLE INTEREST CALCULATOR.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    //SIMPLE INTEREST
+    float principle=5;
+    float rate_of_interest_per_year=2;
+    float year=10;
+    float simple_interest=(principle*rate_of_interest_per_year*year)/100;
+    printf ("%f\n",simple_interest);
+
+   // AMOUNT
+    int s_i=1;
+    int p=5;
+    int amount=s_i+p;
+    printf("%d\n",amount);
+}
