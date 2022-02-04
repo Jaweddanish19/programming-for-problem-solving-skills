ac68e47eba12b0a289068afde5a619c3a9b45fda
MD DANISH JAWED <95688212+Jaweddanish19@users.noreply.github.com>
2022-02-04 21:46:51 +0530

    Add files via upload

---
+++ STUDENT RESULT.c
@@ -0,0 +1,28 @@
+//STUDENT RESULT
+#include <stdio.h>
+
+#include <conio.h>
+
+void main()
+{
+    float physics, chemistry, maths, english;
+    float total;
+    float percentage;
+
+    
+    printf("enter the value of physics: ");
+    scanf("%f", &physics);
+    printf("enter the value of chemistry: ");
+    scanf("%f", &chemistry);
+    printf("enter the value of maths: ");
+    scanf("%f", &maths);
+    printf("enter the value of english: ");
+    scanf("%f", &english);
+    total = physics + chemistry + maths + english;
+    percentage = (total / 400)*100;
+
+
+    printf("%f", percentage);
+
+
+}
